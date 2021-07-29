4bcb8c9e15382e39dc7a7c6219fa8829285c85ed
Lionel Smith <smithlionel1121@gmail.com>
2021-07-29 13:41:25 +0100

    Configure fixture view controller with a collection view

--- MatchReminder/FixtureViewController.swift
+++ MatchReminder/FixtureViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FixtureViewController.swift
 //  MatchReminder
 //
 //  Created by Lionel Smith on 23/07/2021.
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FixtureViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
 
 }
 
