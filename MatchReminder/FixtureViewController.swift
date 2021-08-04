0da42944a1e6bc97dfde2ea611772ffd148cce21
Lionel Smith <smithlionel1121@gmail.com>
2021-08-04 11:35:03 +0100

    Reconfigure fixture view controller with extracted fixture view model

--- MatchReminder/FixtureViewController.swift
+++ MatchReminder/FixtureViewController.swift
@@ -8,39 +8,67 @@
 import UIKit
 
 class FixtureViewController: UIViewController {
+
+    private var collectionView: UICollectionView?
     
-    let apiClient = ApiClient(session: URLSession.shared, resourcePath: "competitions/PL/matches")
+    let fixturesViewModel = FixturesViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        navigationController?.navigationBar.topItem?.title = "Fixtures"
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 1
+        layout.minimumInteritemSpacing = 1
+
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        guard let collectionView = collectionView else {
+            return
+        }
+        
+        collectionView.register(FixtureCollectionViewCell.self, forCellWithReuseIdentifier: fixtureCell)
+        collectionView.backgroundColor = .orange
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+
+        view.addSubview(collectionView)
+        collectionView.frame = view.bounds
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        loadFixtures()
+        fixturesViewModel.loadFixtures()
     }
-    
-    func loadFixtures() {
-        var request = URLRequest(url: apiClient.resourceURL)
-        request.addValue(ApiKey, forHTTPHeaderField: "X-Auth-Token")
-        
-        let dataTask = apiClient.session.dataTask(with: request) { data, response, error in
-
-            if let jsonData = data {
-                let jsonDecoder = JSONDecoder()
-                jsonDecoder.dateDecodingStrategy = .iso8601
-                if let response = try? jsonDecoder.decode(MatchesResponse.self, from: jsonData) {
-                    print(response.matches[0])
-                } else {
-                    print("Failed")
-                }
-            
-            }
-        }
-        dataTask.resume()
-            
-    }
-
 }
 
+extension FixtureViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: fixtureCell, for: indexPath) as! FixtureCollectionViewCell
+        cell.configureCell()
+        return cell
+    }
+}
+
+extension FixtureViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let columns: CGFloat = 1
+        
+        let layout = collectionViewLayout as! UICollectionViewFlowLayout
+        let collectionViewWidth = collectionView.bounds.width
+        let spaceBetweenCells = layout.minimumLineSpacing * (columns - 1)
+        let adjustedWidth = collectionViewWidth - spaceBetweenCells
+        let width: CGFloat = floor(adjustedWidth / columns)
+        let height: CGFloat = 100
+        return CGSize(width: width, height: height)
+    }
+}
