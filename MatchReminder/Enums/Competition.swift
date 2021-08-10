47761e6a9031b5f30a2809b0bed6170ffa9efd9d
Lionel Smith <smithlionel1121@gmail.com>
2021-08-10 11:31:30 +0100

    Add id property to Competition enum

--- MatchReminder/Enums/Competition.swift
+++ MatchReminder/Enums/Competition.swift
@@ -20,4 +20,33 @@ enum Competition: String, CaseIterable {
     case championsLeague = "Champions League"
     case worldCup = "World Cup"
     case europe = "Euros"
+    
+    var id: String {
+        switch self {
+        case .premierLeague:
+            return "PL"
+        case .championship:
+            return "ELC"
+        case .laLiga:
+            return "PD"
+        case .bundesliga:
+            return "BL1"
+        case .serieA:
+            return "SA"
+        case .ligue1:
+            return "FL1"
+        case .eredivisie:
+            return "DED"
+        case .primeriaLiga:
+            return "PPL"
+        case .serieABrazil:
+            return "BSA"
+        case .championsLeague:
+            return "CL"
+        case .worldCup:
+            return "WC"
+        case .europe:
+            return "ECW"
+        }
+    }
 }
