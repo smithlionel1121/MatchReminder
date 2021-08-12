39d01e659b4db1b7510ab9b9f1ead66f02186747
Lionel Smith <smithlionel1121@gmail.com>
2021-08-12 12:50:33 +0100

    Reconfigure fixture view model to enable saving of events to calendar

--- MatchReminder/View Models/FixturesViewModel.swift
+++ MatchReminder/View Models/FixturesViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import EventKit
 
 class FixturesViewModel {
     var competition: Competition {
@@ -22,12 +23,20 @@ class FixturesViewModel {
     
     public var matches: [Match]?
     public var dateGroupedMatches: [Dictionary<Date, [Match?]>.Element]?
+    
+    private let eventStore = EKEventStore()
 
     init(competition: Competition = Competition.allCases[0]) {
         self.competition = competition
         self.resourcePath = "competitions/\(competition.id)/matches"
         self.apiClient = ApiClient(session: URLSession.shared, resourcePath: resourcePath)
         apiClient.queryParams = [URLQueryItem(name: "status", value: "SCHEDULED")]
+
+        requestAccess { (authorized) in
+            if authorized {
+                print("Authorized")
+            }
+        }
     }
     
     func loadFixtures(completion: @escaping (Result<MatchesResponse, ApiError>) -> Void) {
@@ -45,4 +54,60 @@ class FixturesViewModel {
         
         dateGroupedMatches = dateGroup.sorted { $0.key < $1.key }
     }
+}
+
+extension FixturesViewModel {
+    private var isAvailable: Bool {
+        EKEventStore.authorizationStatus(for: .event) == .authorized
+    }
+    
+    private func requestAccess(completion: @escaping (Bool) -> Void) {
+        let currentStatus = EKEventStore.authorizationStatus(for: .event)
+        guard currentStatus == .notDetermined else {
+            completion(currentStatus == .authorized)
+            return
+        }
+        
+        eventStore.requestAccess(to: .event) { (success, Error) in
+            completion(success)
+        }
+    }
+    
+    private func getMatchEvent(with id: Int, completion: (EKEvent?) -> Void) {
+        guard isAvailable else {
+            completion(nil)
+            return
+        }
+        
+        guard let calendarItem = eventStore.calendarItem(withIdentifier: "\(id)"), let ekEvent = calendarItem as? EKEvent else {
+            completion(nil)
+            return
+        }
+        
+        completion(ekEvent)
+    }
+    
+    func saveMatchEvent(_ match: Match, completion: (String?) -> Void) {
+        guard  isAvailable else {
+            return
+        }
+        
+        getMatchEvent(with: match.id) { (ekEvent) in
+            let ekEvent = ekEvent ?? EKEvent(eventStore: self.eventStore)
+            ekEvent.title = "\(match.homeTeam.name) v \(match.awayTeam.name)"
+            ekEvent.startDate = match.utcDate
+            ekEvent.endDate = Calendar.current.date(byAdding: .hour, value: 2, to: match.utcDate)
+            ekEvent.calendar = self.eventStore.defaultCalendarForNewEvents
+            ekEvent.notes = self.competition.rawValue
+            
+            do {
+                try self.eventStore.save(ekEvent, span: .thisEvent)
+                completion(ekEvent.eventIdentifier)
+            } catch {
+                completion(nil)
+            }
+        }
+        
+    }
+    
 }
