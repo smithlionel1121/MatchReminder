c5ffc013c6065b35c307ed46e796d5c33cda3a45
Lionel Smith <smithlionel1121@gmail.com>
2021-08-15 12:23:32 +0100

    Add scores to result collection view cell

--- MatchReminder/Views/ResultCollectionViewCell.swift
+++ MatchReminder/Views/ResultCollectionViewCell.swift
@@ -15,21 +15,47 @@ class ResultCollectionViewCell: FixtureBaseCollectionViewCell {
     private var homeStackView = UIStackView()
     private var awayStackView = UIStackView()
     
+    var homeScoreLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .right
+        return label
+    }()
+    
+    var awayScoreLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .right
+        return label
+    }()
+    
     override func configureContent() {
         contentView.addSubview(matchStackView)
+        matchStackView.addArrangedSubview(homeStackView)
+        matchStackView.addArrangedSubview(awayStackView)
         configureMatchStackView()
+    }
+    
+    override func updateMatch() {
+        if let homeScore = match?.homeScore {
+            homeScoreLabel.text = "\(homeScore)"
+        }
+        if let awayScore = match?.awayScore {
+            awayScoreLabel.text = "\(awayScore)"
+        }
     }
     
     func configureMatchStackView() {
         matchStackView.axis = .vertical
         matchStackView.spacing = 10
         
-        matchStackView.addArrangedSubview(homeStackView)
-        matchStackView.addArrangedSubview(awayStackView)
-        
         setMatchStackViewConstraints()
+        
+        awayLabel.numberOfLines = 1
+        homeLabel.numberOfLines = 1
+        
         homeStackView.addArrangedSubview(homeLabel)
+        homeStackView.addArrangedSubview(homeScoreLabel)
         awayStackView.addArrangedSubview(awayLabel)
+        awayStackView.addArrangedSubview(awayScoreLabel)
 
     }
     
@@ -37,6 +63,7 @@ class ResultCollectionViewCell: FixtureBaseCollectionViewCell {
         matchStackView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             matchStackView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20),
+            matchStackView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -20),
             matchStackView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 30),
             matchStackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -30)
         ])
