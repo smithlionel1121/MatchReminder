86159148ab7c650ddacbf3f3bea25c36a7f67383
Lionel Smith <smithlionel1121@gmail.com>
2021-08-24 15:18:26 +0100

    Load standings data and display content in table view cell

--- MatchReminder/StandingsViewController.swift
+++ MatchReminder/StandingsViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 class StandingsViewController: UIViewController {
     
     let tableView = UITableView()
+    
+    let competitionViewModel = CompetitionViewModel(competitionData: .standings)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTableView()
+        loadStandings()
     }
     
     func configureTableView() {
@@ -35,16 +38,32 @@ class StandingsViewController: UIViewController {
         ])
     }
     
+    func loadStandings() {
+        competitionViewModel.loadData { (result: Result<StandingsResponse, ApiError>) in
+            switch result {
+            case .success(let standingsResponse):            
+                DispatchQueue.main.async { [weak self] in
+                    guard let self = self else { return }
+                    self.competitionViewModel.standings = standingsResponse.standings[0].table
+                    self.tableView.reloadData()
+                }
+            case .failure(let error):
+                print("error \(error)")
+            }
+        }
+    }
+    
 }
 
 extension StandingsViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return competitionViewModel.standings?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: standingsCell, for: indexPath) as! StandingsTableViewCell
-        cell.textLabel?.text = "\(indexPath.row + 1). Team Name"
+        guard let standing = competitionViewModel.standings?[indexPath.row] else { return cell }
+        cell.textLabel?.text = "\(standing.position). \(standing.team.name)"
         return cell
     }
     
